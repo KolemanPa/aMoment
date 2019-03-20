9521113cf7d05483a3f0970584cd5ac6dc9b9ea8
KolemanPa <46792228+KolemanPa@users.noreply.github.com>
2019-03-20 13:31:39 -0500

    some stuff

--- javascript.js
+++ javascript.js
@@ -1,7 +1,23 @@
 // console.log(moment().format('dddd'));
 
-import moment = require("moment");
+var moment = require("moment");
+
+const chalk = require('chalk');
+
+
 
 require("moment");
-var a = moment().format('dddd');
-console.log(a);
+var a = moment().format('HH');
+var c = moment().format('dddd');
+var b = moment().format('HH:MM:s a')
+console.log(`The Time is ${b} on a ${c}`);
+
+if (a > 0 && a < 12){
+    console.log(chalk.red("Good Moring"));
+}
+if (a >=12 && a < 17){
+    console.log(chalk.blue("Good Afternoon"));
+}
+if (a >=17 && a <24){
+    console.log(chalk.yellow("Good night"));
+}
